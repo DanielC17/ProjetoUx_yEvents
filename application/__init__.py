0fb82bc6bf0e1f79cbde5713406bdb6979bd2a36
Daniel Cabral <danielccabral1981@gmail.com>
2021-10-21 22:09:00 -0300

    Setando as rotas

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,8 @@
-from flask import flask
+from flask import Flask
 import os
 
 
-template_folder = os.path.abspath('application/view/templates')
+template_folder = os.path.abspath("application/view/templates")
 static_folder = os.path.abspath("application/view/static")
 
 app = Flask(__name__, template_folder = template_folder, static_folder = static_folder)
--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -1,4 +1,19 @@
 from application import app
 from flask import Flask, render_template, url_for
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/user")
+def user():
+    return render_template("user.html")
+
+@app.route("/calendario")
+def calendario():
+    return render_template("calendario.html")
+
+@app.route("/busca")
+def busca():
+    return render_template("busca.html")
 
