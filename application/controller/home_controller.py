38c3af9ea801820dec06462d23cf1c49bb6b6fab
Daniel Cabral <danielccabral1981@gmail.com>
2021-10-23 14:04:56 -0300

    Base para deixar o site dinamico

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -3,17 +3,16 @@ from flask import Flask, render_template, url_for
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route("/user")
-def user():
-    return render_template("user.html")
-
-@app.route("/calendario")
-def calendario():
-    return render_template("calendario.html")
+    return render_template("eventos.html")
 
 @app.route("/busca")
 def busca():
     return render_template("busca.html")
 
+@app.route("/calendario")
+def calendario():
+    return render_template("calendario.html")
+
+@app.route("/user")
+def user():
+    return render_template("user.html")
