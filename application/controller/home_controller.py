e2984e7324e7e0211a071d1cb88ac26832d7e358
Daniel Cabral <danielccabral1981@gmail.com>
2021-10-24 20:51:54 -0300

    Configurando Button menu

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -1,5 +1,5 @@
 from application import app
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 @app.route("/")
 def index():
@@ -9,10 +9,17 @@ def index():
 def busca():
     return render_template("busca.html")
 
+@app.route('/busca')
+def teste():
+        return redirect(url_for('busca'))
+
 @app.route("/calendario")
 def calendario():
     return render_template("calendario.html")
 
+
 @app.route("/user")
 def user():
     return render_template("user.html")
+
+
